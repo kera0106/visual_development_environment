63a434db94fd6a80252120926fd635f028756564
kera0106 <42892620+kera0106@users.noreply.github.com>
2021-03-28 18:40:35 +0300

    interface prototype created

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_open_clicked()
+{
+    QMessageBox::about(this, "Нажатие кнопки", "Кнопка открыть");
+}
+
+void MainWindow::on_run_clicked()
+{
+    QMessageBox::about(this, "Нажатие кнопки", "Кнопка запуск");
+}
+
+void MainWindow::on_stop_clicked()
+{
+    QMessageBox::about(this, "Нажатие кнопки", "Кнопка стоп");
+}
+
+void MainWindow::on_save_clicked()
+{
+    QMessageBox::about(this, "Нажатие кнопки", "Кнопка сохранить");
+}
